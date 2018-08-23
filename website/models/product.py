c3d7ab294e77e42d0d0507bee2982297818c6bbd
Deanna Vickers <deanna2000@gmail.com>
2018-08-23 14:10:18 -0500

    added category model

--- website/models/product.py
+++ website/models/product.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import User
 from django.db import models
+from .category import Category
 from django.core.validators import MaxValueValidator, MinValueValidator
 
 # Create your models here.
@@ -8,16 +9,17 @@ class Product(models.Model):
     author: Joe Shep
     edited: added category, date_added, rating, like, and location.
     purpose: create product object
+	edited 8/23/18: changed category to be a foregin key - Deanna
     '''
     seller = models.ForeignKey(
         User,
-        on_delete=models.CASCADE,
+        on_delete=models.CASCADE
     )
     title = models.CharField(max_length=255)
     description = models.TextField(blank=True, null=True)
     price = models.IntegerField()
     quantity = models.IntegerField()
-    category = models.CharField(max_length=50)
+    category_name = models.ForeignKey('Category', on_delete=models.CASCADE)
     date_added = models.DateField(auto_now=False, auto_now_add=False)
     rating = models.IntegerField(
            default=1,
@@ -29,3 +31,6 @@ class Product(models.Model):
 
     def __str__(self):
         return self.title
+
+    class Meta:
+        db_table = "products"
